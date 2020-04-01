8aa9735bd48661c527563c77554f2a6a9fe3ff50
JustGeoffrey <s3487069@student.rmit.edu.au>
2020-04-01 20:06:07 +1100

    First commit, already Started some coding

---
+++ src/dbload.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+public class dbload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int pagesize = 0 ;
+		
+		if(args.length == 3) {
+			if(args[0] == "-p") {
+				
+			    try {
+			        pagesize = Integer.parseInt(args[1]);
+			    } catch (NumberFormatException nfe) {
+					throw new NumberFormatException("Second Arguement must be an integer");
+			    }
+				
+			    //Process the datafile
+			    
+			    
+			} else {
+				System.out.println("First Arguement should be -p");
+			}
+		} else {
+			System.out.println("Incorrect arguements, correct format '-p pagesize(integer) datafile.csv'");
+		}
+	}
+
+}
